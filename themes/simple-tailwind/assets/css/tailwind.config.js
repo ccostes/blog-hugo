5f6eb036839d9012ecd02cb0cd09300dbd5440e8
Chris Costes <costes.c@gmail.com>
2024-04-07 20:54:42 -0400

    Change to tailwind typography with misc updates.

--- themes/simple-tailwind/assets/css/tailwind.config.js
+++ themes/simple-tailwind/assets/css/tailwind.config.js
@@ -1,3 +1,11 @@
+
+const round = (num) =>
+  num
+    .toFixed(7)
+    .replace(/(\.[0-9]+?)0+$/, '$1')
+    .replace(/\.0$/, '')
+const rem = (px) => `${round(px / 16)}rem`
+const em = (px, base) => `${round(px / base)}em`
 module.exports = {
   darkMode: 'class',
   content: [
@@ -7,6 +15,44 @@ module.exports = {
     "./content/**/*.html"
   ],
   theme: {
+    extend: {
+      typography: {
+        lg: {
+          css: {
+            fontSize: '1.3rem',
+            h1: {
+              fontWeight: 'normal',
+              fontSize: '2.25rem',
+              a: {
+                textDecoration: 'none',
+              }
+            },
+            h2: {
+              fontWeight: 'normal',
+              a: {
+                textDecoration: 'none',
+              }
+            },
+            h3: {
+              fontWeight: 'normal',
+              a: {
+                textDecoration: 'none',
+              }
+            },
+            h4: {
+              fontWeight: 'normal',
+              a: {
+                textDecoration: 'none',
+              }
+            },
+          },
+        },
+        invert: {
+          css: {
+          }
+        }
+      },
+    },
     fontFamily: {
       serif: ['et-book', 'Palatino', "Palatino Linotype", "Palatino LT STD", "Book Antiqua", 'Georgia', 'serif']
     },
@@ -19,12 +65,10 @@ module.exports = {
       '4xl': '2.441rem',
       '5xl': '3.052rem',
     },
-    extend: {
-    },
   },
   plugins: [
     // require('@tailwindcss/aspect-ratio'),
     // require('@tailwindcss/forms'),
-    // require('@tailwindcss/typography'),
+    require('@tailwindcss/typography'),
   ]
 }
